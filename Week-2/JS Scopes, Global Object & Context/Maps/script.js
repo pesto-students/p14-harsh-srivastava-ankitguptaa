a6f792d0f77b9bbd0eb07940d25aea89b7dda162
Ankit Gupta10 <ankit.gupta10@ibm.com>
2023-12-29 14:43:31 +0530

    Map exercises added.

--- Week-2/JS Scopes, Global Object & Context/Maps/script.js
+++ Week-2/JS Scopes, Global Object & Context/Maps/script.js
@@ -1,3 +1,16 @@
-function calcWordFrequencies() {
+function calcWordFrequencies(input) {
+    let wordArray = input.split(" ");
+    let map = new Map();
+    for (let index = 0; index < wordArray.length; index++) {
+        const element = wordArray[index];
+        if(map.has(element)) {
+            map.set(element, map.get(element) + 1);
+        } else {
+            map.set(element, 1);
+        }
+    }
 
+    return map;
 }
+
+console.log(calcWordFrequencies("hey hi Mark hi mark"));
